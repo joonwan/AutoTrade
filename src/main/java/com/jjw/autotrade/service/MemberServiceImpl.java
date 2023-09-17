031e66cc18431f58c03033c0752935669bc8e35c
joonwan <joonwan1024@naver.com>
2023-09-18 02:17:39 +0900

    member service 구현체 생성

---
+++ src/main/java/com/jjw/autotrade/service/MemberServiceImpl.java
@@ -0,0 +1,26 @@
+package com.jjw.autotrade.service;
+
+import com.jjw.autotrade.domain.Member;
+import com.jjw.autotrade.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberServiceImpl implements MemberService{
+
+
+    private final MemberRepository repository;
+
+
+    @Override
+    public Member join(Member member) {
+        repository.save(member);
+        return member;
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return  repository.findById(memberId);
+    }
+}
