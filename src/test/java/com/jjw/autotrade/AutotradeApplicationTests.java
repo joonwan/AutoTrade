679a42bca67f89a6c648108d10761ecde9aaa74e
joonwan <joonwan1024@naver.com>
2023-09-18 01:02:56 +0900

    create project

---
+++ src/test/java/com/jjw/autotrade/AutotradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjw.autotrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutotradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
