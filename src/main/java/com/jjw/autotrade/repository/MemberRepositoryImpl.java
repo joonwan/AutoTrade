1432f31d34d6fdab5272cbdd5f7af09cc307d895
joonwan <joonwan1024@naver.com>
2023-09-18 02:10:38 +0900

    member repository test사항 추가

--- src/main/java/com/jjw/autotrade/repository/MemberRepositoryImpl.java
+++ src/main/java/com/jjw/autotrade/repository/MemberRepositoryImpl.java
@@ -4,8 +4,9 @@ import com.jjw.autotrade.domain.Member;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
-
+@Transactional // service 계층 생성후 제거할 예정
 @Repository
 public class MemberRepositoryImpl implements MemberRepository{
 
--- src/test/java/com/jjw/autotrade/AutotradeApplicationTest.java
+++ src/test/java/com/jjw/autotrade/AutotradeApplicationTest.java
@@ -1,4 +1,7 @@
+package com.jjw.autotrade;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class AutotradeApplicationTest {
-  
+
 }
